24f665c60346de832aeaaa0ab9b4ee12b82c7057
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-21 21:54:53 +0530

    Update registration.php
    added some jquery and alert.

--- registration.php
+++ registration.php
@@ -42,26 +42,54 @@ require_once('config.php');
 
 					<hr class="mb-3">
 					<label for="firstname"><h3>First Name</h3></label>
-					<input class="form-control" type="text" name="firstname" required="">
+					<input class="form-control" id="firstname" type="text" name="firstname" required="">
 
 					<label for="lastname"><h3>Last Name</h3></label>
-					<input class="form-control" type="text" name="lastname" required="">
+					<input class="form-control" id="lastname" type="text" name="lastname" required="">
 
 					<label for="email"><h3>Email</h3></label>
-					<input class="form-control" type="email" name="email" required="">
+					<input class="form-control" id="email" type="email" name="email" required="">
 
 					<label for="phone"><h3>Phone</h3></label>
-					<input class="form-control" type="text" name="phone" required="">
+					<input class="form-control" id="phone" type="text" name="phone" required="">
 
 					<label for="password"><h3>Password</h3></label>
-					<input class="form-control" type="password" name="password" required="">
+					<input class="form-control" id="password" type="password" name="password" required="">
 					<hr class="mb-3">
 
-					<input class="btn btn-primary" type="submit" name="create" value="Sign Up">
+					<input class="btn btn-primary" id="register" type="submit" name="create" value="Sign Up">
 					</div>
 				</div>
 			</div>
 		</form>
 	</div>
+<!--copy jquery script and paste here-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!--copy sweet alert-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script type="text/javascript">
+	$(function(){
+		$('#register').click(function(e)){
+			var valid = this.form.checkValidity();
+			if(valid){
+				e.preventDefault();
+				alert('true');
+			}else{
+				alert('false');
+			}
+			var firstname =$('#firstname').val();
+			var lastname =$('#lastname').val();
+			var email =$('#email').val();
+			var phone =$('#phone').val();
+			var password =$('#password').val();
+		}
+		//alert('hello');
+		swal.fire({
+			'title': 'Congratulation',
+			'text':'Form Succefully Feed',
+			'type': 'success'
+		})
+	});
+</script>
 </body>
 </html>
