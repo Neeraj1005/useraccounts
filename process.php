afaaf4bb46439a09c7c54e5f702e6899c62b4497
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-22 11:33:32 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,25 +1,24 @@
-<?php 
+<?php
 require_once('config.php');
 ?>
-<?php 
-if(isset($_POST)){
-			$firstname 		=$_POST['firstname'];
-			$lastname 		=$_POST['lastname'];
-			$email 			=$_POST['email'];
-			$phone 			=$_POST['phone'];
-			$password 		=sha1($_POST['password']);
+<?php
 
-			//Begin sql feed
-			$sql = "INSERT INTO users (firstname, lastname, email, phone, password) VALUES(?,?,?,?,?)";
-			$stmtinsert = $db->prepare($sql);
-			$result = $stmtinsert->execute([$firstname, $lastname, $email, $phone, $password]);
-			if($result){
-				echo "Succefully feed";
-			}else{
-				echo "There is an error";
-			}
-			//End sql feed
-			//echo $firstname ."" . $lastname ."" . $email ."" . $password;
+if(isset($_POST)){
+
+	$firstname 		= $_POST['firstname'];
+	$lastname 		= $_POST['lastname'];
+	$email 			= $_POST['email'];
+	$phone			= $_POST['phone'];
+	$password 		= sha1($_POST['password']);
+
+		$sql = "INSERT INTO users (firstname, lastname, email, phone, password ) VALUES(?,?,?,?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$result = $stmtinsert->execute([$firstname, $lastname, $email, $phone, $password]);
+		if($result){
+			echo 'Successfully saved.';
+		}else{
+			echo 'There were erros while saving the data.';
+		}
 }else{
-	echo "No Data";
+	echo 'No data';
 }
