6af0c8cb7c8d0cf16b83fc6b66b1ea30bbaaa229
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-22 11:33:38 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,98 +1,105 @@
-<?php 
+<?php
 require_once('config.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration Form</title>
+	<title>User Registration | PHP</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<div>
-		<?php
-			//content move to process.php		
-		?>
-	</div>
-	<div>
-		<form action="registration.php" method="post">
-			<div class="container">
 
-				<div class="row">
-					<div class="col-sm 3">
+<div>
+	<?php
+	
+	?>	
+</div>
+
+<div>
+	<form action="registration.php" method="post">
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-sm-3">
 					<h1>Registration</h1>
-					<p>Fill up the form with validation</p>
-
+					<p>Fill up the form with correct values.</p>
 					<hr class="mb-3">
-					<label for="firstname"><h3>First Name</h3></label>
-					<input class="form-control" id="firstname" type="text" name="firstname" required="">
+					<label for="firstname"><b>First Name</b></label>
+					<input class="form-control" id="firstname" type="text" name="firstname" required>
 
-					<label for="lastname"><h3>Last Name</h3></label>
-					<input class="form-control" id="lastname" type="text" name="lastname" required="">
+					<label for="lastname"><b>Last Name</b></label>
+					<input class="form-control" id="lastname"  type="text" name="lastname" required>
 
-					<label for="email"><h3>Email</h3></label>
-					<input class="form-control" id="email" type="email" name="email" required="">
+					<label for="email"><b>Email Address</b></label>
+					<input class="form-control" id="email"  type="email" name="email" required>
 
-					<label for="phone"><h3>Phone</h3></label>
-					<input class="form-control" id="phone" type="text" name="phone" required="">
+					<label for="phone"><b>Phone Number</b></label>
+					<input class="form-control" id="phone"  type="text" name="phone" required>
 
-					<label for="password"><h3>Password</h3></label>
-					<input class="form-control" id="password" type="password" name="password" required="">
+					<label for="password"><b>Password</b></label>
+					<input class="form-control" id="password"  type="password" name="password" required>
 					<hr class="mb-3">
-
-					<input class="btn btn-primary" id="register" type="submit" name="create" value="Sign Up">
-					</div>
+					<input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
 				</div>
 			</div>
-		</form>
-	</div>
-<!--copy jquery script and paste here-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<!--copy sweet alert-->
+		</div>
+	</form>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 <script type="text/javascript">
 	$(function(){
 		$('#register').click(function(e){
-			var valid = this.form.checkValidity();
 
+			var valid = this.form.checkValidity();
 
 			if(valid){
 
-			var firstname =$('#firstname').val();
-			var lastname =$('#lastname').val();
-			var email =$('#email').val();
-			var phone =$('#phone').val();
-			var password =$('#password').val();
 
-				e.preventDefault();
+			var firstname 	= $('#firstname').val();
+			var lastname	= $('#lastname').val();
+			var email 		= $('#email').val();
+			var phone = $('#phone').val();
+			var password 	= $('#password').val();
+			
+
+				e.preventDefault();	
 
 				$.ajax({
 					type: 'POST',
 					url: 'process.php',
-					data: {firstname: firstname, lastname: lastname, email: email,phone: phone, password: password},
+					data: {firstname: firstname,lastname: lastname,email: email,phone: phone,password: password},
 					success: function(data){
-						Swal.fire({
-						'title': 'Congratulation',
-						'text':data,
-						'type': 'success'
+					Swal.fire({
+								'title': 'Successful',
+								'text': data,
+								'type': 'success'
 								})
+							
 					},
 					error: function(data){
 						Swal.fire({
-						'title': 'Error',
-						'text':'error in form',
-						'type': 'error'
+								'title': 'Errors',
+								'text': 'There were errors while saving the data.',
+								'type': 'error'
 								})
 					}
 				});
 
+				
 			}else{
 				
 			}
+
 			
-		});
-		//alert('hello');
+
+
+
+		});		
+
 		
 	});
+	
 </script>
 </body>
 </html>
